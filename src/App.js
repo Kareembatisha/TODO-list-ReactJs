2f1dd7542d696194a102f12adcc4cb2adc4b78fe
kareem batisha <kareembatisha39@gmail.com>
2024-05-02 09:51:59 +0300

    make add logic and do some style

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
   const [x, setx] = useState([]);
+  const inputRef = useRef();
+
+  const add = () => {
+    const value = inputRef.current.value
+    setx([...x,value])
+    inputRef.current.value = ""
+  };
 
   return (
     <div className="App">
       <h2>TODO List</h2>
 
       <ul>
-        {x.map((item) => {
-          return <li>{item}</li>;
+        {x.map((item,index) => {
+          return <li key={index}>{item}</li>;
         })}
       </ul>
 
-      <input placeholder="enter new task ..." />
+      <input ref={inputRef} placeholder="enter new task ..." />
 
-      <button>Add</button>
+      <button onClick={add}>Add</button>
     </div>
   );
 }
