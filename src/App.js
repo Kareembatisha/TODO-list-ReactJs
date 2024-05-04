984a38c524ff55c08ab2d9ad91eaa007b7daadfd
kareem batisha <kareembatisha39@gmail.com>
2024-05-04 10:23:10 +0300

    add delete function

--- src/App.js
+++ src/App.js
@@ -6,24 +6,46 @@ function App() {
   const inputRef = useRef();
 
   const add = () => {
-    const value = inputRef.current.value
-    setx([...x,value])
-    inputRef.current.value = ""
+    const value = inputRef.current.value;
+    if(!value) return alert("Add the Task")
+    const newData = { completed: false, value };
+    setx([...x, newData]);
+    inputRef.current.value = "";
   };
-
+  const itemDone = (index) => {
+    const newX = [...x];
+    x[index].completed = !x[index].completed;
+    setx(newX);
+  };
+  const delItem = (index) => {
+    const newX = [...x];
+    newX.splice(index, 1);
+    setx(newX);
+  };
   return (
     <div className="App">
       <h2>TODO List</h2>
 
-      <ul>
-        {x.map((item,index) => {
-          return <li key={index}>{item}</li>;
-        })}
-      </ul>
-
       <input ref={inputRef} placeholder="enter new task ..." />
 
       <button onClick={add}>Add</button>
+      <ul>
+        {x.map(({ value, completed }, index) => {
+          return (
+            <div className="main">
+              <li
+                className={completed ? "diffstyle" : ""}
+                key={index}
+                onClick={() => itemDone(index)}
+              >
+                {completed ? "✅" : "⏰"}
+                {value}
+              </li>
+              <span onClick={() => delItem(index)}>❌</span>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 }
